75f730bcde80e53480ca187333d1f0e51e550658
Jen Triñanes <jen.trinanes@whitecobalt.com>
2024-10-30 17:56:26 +0800

    Create Custom Domain for a CDN Endpoint

--- main.tf
+++ main.tf
@@ -56,3 +56,23 @@ resource "azurerm_cdn_endpoint" "cdncrcjen" {
     host_name = "sacrcjen.z23.web.core.windows.net"
   }
 }
+
+# Create Custom Domain for a CDN Endpoint
+data "azurerm_dns_zone" "resume-jtrinanes-com" {
+  name                = "resume.jtrinanes.com"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_dns_cname_record" "resume-jtrinanes-com" {
+  name                = "resume-jtrinanes-com"
+  zone_name           = data.azurerm_dns_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  target_resource_id  = azurerm_cdn_endpoint.cdncrcjen.id
+}
+
+resource "azurerm_cdn_endpoint_custom_domain" "resume-jtrinanes-com" {
+  name            = "resume-jtrinanes-com"
+  cdn_endpoint_id = azurerm_cdn_endpoint.cdncrcjen.id
+  host_name       = "${azurerm_dns_cname_record_resume-jtrinanes-com.name}.${data.azurerm_dns_zone.resume-jtrinanes-com.name}"  
+}
